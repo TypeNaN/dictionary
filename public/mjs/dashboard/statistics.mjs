3a1a3c4dd7401f7e13708da3b1bf1c5b0a925c69
TypeNaN <krajaymail@gmail.com>
2022-09-14 00:44:14 +0700

    Render mockup before fetching data (faster)

--- public/mjs/dashboard/statistics.mjs
+++ public/mjs/dashboard/statistics.mjs
@@ -6,8 +6,6 @@ export default class {
   constructor(container, socket) {
     this.container = container
     
-    // socket.emit('word-stat', null)
-
     socket.on('word-stat-error', (data) => console.error(data))
     socket.on('word-view-error', (data) => console.error(data))
     socket.on('word-views-error', (data) => console.error(data))
@@ -18,7 +16,7 @@ export default class {
     socket.on('word-patch-error', (data) => console.error(data))
     socket.on('word-patch-key-error', (data) => console.error(data))
     
-    socket.on('word-stat-success', (data) => console.log(data))
+    socket.on('word-stat-success', (data) => this.renderStat(data.result))
     socket.on('word-view-success', (data) => console.log(data))
     socket.on('word-views-success', (data) => console.log(data))
     socket.on('word-search-success', (data) => console.log(data))
@@ -46,92 +44,18 @@ export default class {
   }
 
   fetch = async () => {
-    const res = await fetch('/statistics', { method: 'GET' }).catch((e) => console.error(e))
-    if (res.status > 200) {
-      const data = await res.text()
-      console.error(data)
-      if (!document.getElementById('statistics')) this.mockup()
-      return
-    }
-    const data = await res.json()
-    this.render(data)
+    fetch('/statistics', { method: 'GET' }).then((res) => {
+      if (res.status == 200) return res.json().then((data) => this.renderStat(data))
+      res.text().then((message)=>console.log(message))
+    }).catch((err) => console.error(err))
   }
 
-  render = async (data) => {
-    const statistics = document.createElement('div')
-    statistics.className = 'statistics'
-    statistics.id = 'statistics'
-
-    const total = document.createElement('div')
-    total.className = 'statistics-total'
-    total.innerHTML = `มีทั้งหมด ${data.total} คำศัพท์ในพจนานุกรม`
-    this.container.appendChild(total)
-
-    const fsection = document.createElement('section')
-    const flabel = document.createElement('div')
-    const fbox = document.createElement('div')
-    const fvalue = document.createElement('div')
-    const first = document.createElement('div')
-    fsection.className = 'statistics-section'
-    flabel.className = 'statistics-label'
-    fbox.className = 'statistics-box'
-    fvalue.className = 'statistics-value'
-    flabel.innerHTML = 'คำศัพท์แรกของพจนานุกรม'
-    let html = `<div><div>คำศัพท์</div> ${data.first.name}</div>`
-    html += `<div><div>เพิ่มเมื่อ</div> ${thdate(new Date(data.first.create), { time: true })}</div>`
-    html += `<div><div>แก้ไขเมื่อ</div> ${thdate(new Date(data.first.modified), { time: true })}</div>`
-    html += `<div><div>เชื่อมโยงมา</div> ${data.first.previous.length} คำศัพท์</div>`
-    html += `<div><div>เชื่อมโยงไป</div> ${data.first.next.length} คำศัพท์</div>`
-    first.innerHTML = html
-    fvalue.appendChild(first)
-    fbox.appendChild(fvalue)
-    fsection.appendChild(flabel)
-    fsection.appendChild(fbox)
-    statistics.appendChild(fsection)
-
-    new Array(
-      ['lastHigh', data.lastHigh, 'modified', 'คำศัพท์ที่พบบ่อย'],
-      ['lastLow', data.lastLow, 'modified', 'คำศัพท์ที่อาจถูกลืม'],
-      ['lastAdd', data.lastAdd, 'create', 'คำศัพท์ที่ถูกเพิ่มล่าสุด'],
-      ['lastMod', data.lastMod, 'modified', 'คำศัพท์ที่ถูกแก้ไขล่าสุด'],
-      ['lastDel', data.lastDel, 'modified', 'คำศัพท์ที่ถูกลบล่าสุด']
-    ).forEach((x) => {
-      const section = document.createElement('section')
-      const label   = document.createElement('div')
-      const box     = document.createElement('div')
-      const value   = document.createElement('div')
-      section.id    = x[0]
-      section.className = 'statistics-section'
-      label.className   = 'statistics-label'
-      box.className     = 'statistics-box'
-      value.className   = 'statistics-value'
-      label.innerHTML   = x[3]
-      value.appendChild(this.extract(x[1], x[2]))
-      box.appendChild(value)
-      section.appendChild(label)
-      section.appendChild(box)
-      statistics.appendChild(section)
-    })
-    this.container.appendChild(statistics)
-  }
-
-  extract = (buf, timestamps = 'create') => {
-    const ul = document.createElement('ul')
-    buf.forEach((word) => {
-      const li = document.createElement('li')
-      let tooltip = `คำศัพท์ ${word.name}\n`
-      tooltip += `เพิ่มเมื่อ ${thdate(new Date(word['create']), { time: true, full: true, thainum: true })}\n`
-      tooltip += `แก้ไขเมื่อ ${thdate(new Date(word['modified']), { time: true, full: true, thainum: true })}\n`
-      tooltip += `เชื่อมโยงมา ${thnum(word.previous.length)} คำศัพท์\n`
-      tooltip += `เชื่อมโยงไป ${thnum(word.next.length)} คำศัพท์`
-      li.setAttribute('title', tooltip)
-      li.innerHTML = `<div class='date'>${thdate(new Date(word[timestamps]), { time: true })}</div>`
-      li.innerHTML += `<div class='previous'>${word.previous.length}</div>`
-      li.innerHTML += `<div class='word'>${word.name}</div>`
-      li.innerHTML += `<div class='next'>${word.next.length}</div>`
-      ul.appendChild(li)
-    })
-    return ul
+  render = async (socket) => {
+    this.mockup()
+    // support fetch and socket
+    // this.fetch()
+    socket.emit('word-stat', null)
+    return this
   }
 
   mockup = () => {
@@ -188,6 +112,52 @@ export default class {
       statistics.appendChild(section)
     })
     this.container.appendChild(statistics)
+  }
+
+  renderStat = async (data) => {
+    const total = document.getElementsByClassName('statistics-total')[0]
+    total.textContent = `มีทั้งหมด ${data.total} คำศัพท์ในพจนานุกรม`
+    const statistics = document.getElementById('statistics')
+    const fsection = statistics.childNodes[0]
+    const fbox = fsection.childNodes[1]
+    const fvalue = fbox.childNodes[0]
+    const first = fvalue.childNodes[0]
+    let html = `<div><div>คำศัพท์</div> ${data.first.name}</div>`
+    html += `<div><div>เพิ่มเมื่อ</div> ${thdate(new Date(data.first.create), { time: true })}</div>`
+    html += `<div><div>แก้ไขเมื่อ</div> ${thdate(new Date(data.first.modified), { time: true })}</div>`
+    html += `<div><div>เชื่อมโยงมา</div> ${data.first.previous.length} คำศัพท์</div>`
+    html += `<div><div>เชื่อมโยงไป</div> ${data.first.next.length} คำศัพท์</div>`
+    first.innerHTML = html
+    new Array(
+      ['lastHigh', data.lastHigh, 'modified', 'คำศัพท์ที่พบบ่อย'],
+      ['lastLow', data.lastLow, 'modified', 'คำศัพท์ที่อาจถูกลืม'],
+      ['lastAdd', data.lastAdd, 'create', 'คำศัพท์ที่ถูกเพิ่มล่าสุด'],
+      ['lastMod', data.lastMod, 'modified', 'คำศัพท์ที่ถูกแก้ไขล่าสุด'],
+      ['lastDel', data.lastDel, 'modified', 'คำศัพท์ที่ถูกลบล่าสุด']
+    ).forEach((x) => {
+      const section = document.getElementById(x[0])
+      const box = section.childNodes[1]
+      const value = box.childNodes[0]
+      const ul = value.childNodes[0]
+      this.extract(ul, x[1], x[2])
+    })
+  }
+
+  extract = (parent, buf, timestamps = 'create') => {
+    buf.forEach((word) => {
+      const li = document.createElement('li')
+      let tooltip = `คำศัพท์ ${word.name}\n`
+      tooltip += `เพิ่มเมื่อ ${thdate(new Date(word['create']), { time: true, full: true, thainum: true })}\n`
+      tooltip += `แก้ไขเมื่อ ${thdate(new Date(word['modified']), { time: true, full: true, thainum: true })}\n`
+      tooltip += `เชื่อมโยงมา ${thnum(word.previous.length)} คำศัพท์\n`
+      tooltip += `เชื่อมโยงไป ${thnum(word.next.length)} คำศัพท์`
+      li.setAttribute('title', tooltip)
+      li.innerHTML = `<div class='date'>${thdate(new Date(word[timestamps]), { time: true })}</div>`
+      li.innerHTML += `<div class='previous'>${word.previous.length}</div>`
+      li.innerHTML += `<div class='word'>${word.name}</div>`
+      li.innerHTML += `<div class='next'>${word.next.length}</div>`
+      parent.appendChild(li)
+    })
   }
 
   insertLastStat = async (data, options) => {
